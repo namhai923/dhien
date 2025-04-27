2d32b8bc273db165a76f5659b191156e0c3cd7d6
Hai Nguyen <namhainguyenminh@gmail.com>
2025-04-27 15:34:16 +0700

    fix config file

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
-  name: "Portfolio Template",
-  description: "A portfolio website template",
+  name: "Dao Hien",
+  description: "Dao Hien's Portfolio Website",
   links: {
     tailwindcss: "https://tailwindcss.com/",
     shadcn: "https://ui.shadcn.com/",
